a8921994e234a766cf402ec5d6b2ff74bdf95c5f
Chirag Kanani <chiragkanani144@gmail.com>
2024-04-13 14:41:55 +0530

    9

--- utils/verifyuser.js
+++ utils/verifyuser.js
@@ -1,8 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  console.log(token);
+  function getCookie(name) {
+    const cookieString = document.cookie;
+    const cookies = cookieString.split("; ");
+
+    for (const cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.split("=");
+      if (cookieName === name) {
+        return decodeURIComponent(cookieValue);
+      }
+    }
+
+    return null;
+  }
+  const token = getCookie("access_token");
   if (!token) {
     return res.json({ success: false, msg: "Unauthorized.!!" });
   }
